f12505e148d5fa2f95c7d5d764f9092ce38935fd
bakdoolot <altybaevbaku15@gmail.com>
2023-11-07 19:36:59 +0600

    Edit main page

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,14 @@
+import { observer } from "mobx-react-lite";
+
+const TaskList = observer(() => {
+  return (
+    <div>
+      <div className="flex flex-col sm:flew-row gap-4 sm:items-center sm:justify-between mb-8 sm:mb-14">
+        <h2 className="text-2xl font-semibold">All The{" "}
+        </h2>
+      </div>
+    </div>
+  );
+});
+
+export default TaskList;
